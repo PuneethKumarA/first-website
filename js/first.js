759db25c37ed3d86ee68933c7252e820a3cab546
PuneethKumarA <apuneethk@gmail.com>
2021-11-12 03:14:51 -0800

    initial javascript

---
+++ js/first.js
@@ -0,0 +1,15 @@
+function myFunction(a, b) {
+    console.log("the answer is" + a*b)
+    return a * b;
+  } 
+  
+
+  function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
